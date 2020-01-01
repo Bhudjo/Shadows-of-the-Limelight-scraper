59ee697dfaede4f44a85f69c7ed782a54048d137
Alessandro Buggin <alessandro.buggin@gmail.com>
2020-01-01 20:42:21 +0000

    Create only one file

--- downloader.py
+++ downloader.py
@@ -7,14 +7,19 @@ header = {"User-Agent": "Mozilla - stocazzo"}
 html = requests.get(url, headers=header).text
 soup = BeautifulSoup(html, 'html.parser')
 content = soup.find("div", class_='entry-content')
-for child in content.descendants:
-    if child.name == 'a':
-      href = child.attrs['href']
-      title = child.contents[0]
-      encoded_title = urllib.parse.quote_plus(title)
-      with open(f"{encoded_title}.html",'w') as target:
-        html = requests.get(href, headers=header).text
-        ch_soup = BeautifulSoup(html, 'html.parser')
-        body = ch_soup.find("div", class_='entry-content').get_text()
-        target.writelines(body)
+body = ""
+with open(f"Shadows-of-the-Limelight.html",'w') as target:
+    for child in content.descendants:
+        if child.name == 'a':
+            href = child.attrs['href']
+            title = child.contents[0]
+            html = requests.get(href, headers=header).text
+            ch_soup = BeautifulSoup(html, 'html.parser')
+            body += f"<h1>{title}</h1>"
+            extracted = ch_soup.find("div", class_='entry-content').get_text()
+            prev = "Previously …"
+            next_ = "Next …"
+            body += extracted.replace(prev,"").replace(next_,"")
+            print(f"{title}: done")
+    target.writelines(body)
 
